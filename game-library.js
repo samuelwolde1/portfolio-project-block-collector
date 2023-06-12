cb48a4e360be41b056bfd400daa16e4e3c016fee
samuelwolde1 <126281252+samuelwolde1@users.noreply.github.com>
2023-06-12 11:17:55 -0600

    Add files via upload

---
+++ game-library.js
@@ -0,0 +1,71 @@
+// GAME LIBRARY for HTML CANVAS
+
+// GLOBAL VARIABLES
+let mouseX;
+let mouseY;
+
+let keyPressed = {};
+
+// EVENT STUFF
+
+// Update position of mouse, (mouseX, mouseY), when mouse moved
+document.addEventListener("mousemove", mousemoveHandlerGameLib);
+
+function mousemoveHandlerGameLib(e) {
+  // Get rectangle info about canvas location
+  let cnvRect = cnv.getBoundingClientRect();
+
+  // Calc mouse coordinates using mouse event and canvas location info
+  mouseX = Math.round(e.clientX - cnvRect.left);
+  mouseY = Math.round(e.clientY - cnvRect.top);
+}
+
+// Update keyPressed object on keydown (e.code: true)
+document.addEventListener("keydown", keydownHandlerGameLib);
+
+function keydownHandlerGameLib(e) {
+  keyPressed[e.code] = true;
+}
+
+// Update keyPressed object on keyup (e.code: false)
+document.addEventListener("keyup", keyupHandlerGameLib);
+
+function keyupHandlerGameLib(e) {
+  keyPressed[e.code] = false;
+}
+
+// USEFUL GAME FUNCTIONS
+
+// Determine the distance between (x1, y1) and (x2, y2)
+function dist(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
+// Determine if point (x, y) is in rect object (x, y, w, h)
+function ptInRect(x, y, rect) {
+  return (
+    x >= rect.x &&
+    x <= rect.x + rect.w &&
+    y >= rect.y &&
+    y <= rect.y + rect.h
+  );
+}
+
+// Determine if two rect objects (x, y, w, h) collide
+function rectCollide(rect1, rect2) {
+  return (
+    rect1.x < rect2.x + rect2.w &&
+    rect1.x + rect1.w > rect2.x &&
+    rect1.y < rect2.y + rect2.h &&
+    rect1.y + rect1.h > rect2.y
+  );
+}
+
+// Determine if point (x, y) is in circle object (x, y, r)
+function ptInCircle(x, y, circle) {}
+
+// Determine if two circle objects (x, y, r) collide
+function circleCollide(circle1, circle2) {}
+
+// Constrain val so that it must be between low and high
+function constrain(val, low, high) {}
---
+++ main.js
@@ -0,0 +1,63 @@
+// JS GAME SKELETON
+
+// CANVAS SETUP
+let cnv = document.getElementById("my-canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// GLOBAL VARIABLES
+let state = "start";
+let player = {
+  x: 388,
+  y: 288,
+  w: 25,
+  h: 25,
+  color: "blue",
+  speed: 5,
+};
+let blocks = [];
+let redBlocks = [];
+let timer = 0;
+
+
+// START DRAW FUNCTION ON PAGE LOAD
+window.addEventListener("load", draw);
+
+function draw() {
+  // GAME STATE
+  if (state === "start") {
+    startScreen();
+  } else if (state === "running") {
+    gameLogic();
+    gameScreen();
+  } else if (state === "gameover") {
+    gameOver();
+  }
+
+  // REDRAW
+  requestAnimationFrame(draw);
+}
+
+// EVENT STUFF
+
+// KEYDOWN EVENT
+document.addEventListener("keydown", keydownHandler);
+
+function keydownHandler(e) {
+  if (state === "start" && e.code === "Space") {
+    state = "running";
+  } else if (state === "gameover" && e.code === "Space") {
+    reset();
+  }
+
+  if (state === "running"){
+    if (e.code === "ArrowLeft" || e.code === "ArrowRight" || e.code === "ArrowUp" || e.code === "ArrowDown") {
+      for (let i = 0; i < blocks.length; i++) {
+        if (rectCollide(player, blocks[i])) {
+          blocks.splice(i, 1);
+        }
+      }
+    }
+  }
+}
